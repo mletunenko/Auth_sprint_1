2b0c6615148e256a2e28641958a694aaaf82de5b
mletunenko <mletunenko@gmail.com>
2024-12-11 16:11:38 +0300

    добавлен базовый класс моделей для алхимии

---
+++ src/models/base.py
@@ -0,0 +1,22 @@
+import uuid
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr
+from sqlalchemy.testing.schema import mapped_column
+
+from utils.case_converter import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False
+    )
