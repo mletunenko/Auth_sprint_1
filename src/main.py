1bbf3886d552b8f34f7d7d9255ded0cbf93b992f
mletunenko <mletunenko@gmail.com>
2024-12-11 15:58:23 +0300

    в main добавлен помошник лдля работы с postgres

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
+from db.postgres import pg_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # старт
+    yield
+    # стоп
+    await pg_helper.dispose()
+
+
+app = FastAPI(
+    lifespan=lifespan,
+)
 app.include_router(
     api_router,
     prefix=settings.api.prefix,
