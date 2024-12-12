03cfc0d0ee018593ca6395ee306358b60b72dbcc
mletunenko <mletunenko@gmail.com>
2024-12-12 10:52:12 +0300

    добавление поля users.email

--- src/models/user.py
+++ src/models/user.py
@@ -8,12 +8,14 @@ from .base import Base
 class User(Base):
     login: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[bytes] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
     first_name: Mapped[str] = mapped_column(nullable=True)
     last_name: Mapped[str] = mapped_column(nullable=True)
     created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now(datetime.UTC))
 
-    def __init__(self, login: str, password:str, first_name: str, last_name: str) -> None:
+    def __init__(self, login: str, email: str, password:str, first_name: str, last_name: str) -> None:
         self.login = login
+        self.email = email
         self.password = self.password = generate_password_hash(password)
         self.first_name = first_name
         self.last_name = last_name
