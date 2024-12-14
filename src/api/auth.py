5945a2662d85b8672f3f75a5d64867ba5c996e0f
mletunenko <mletunenko@gmail.com>
2024-12-14 14:24:24 +0300

    добавлена проверка в create_user, что на уникальность login и email нового юзера

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
+from sqlalchemy import or_
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from db.postgres import pg_helper
+from models import User
 from schemas.user import UserBaseOut, UserCreateIn
 from services.users import create_user as services_create_user
 
@@ -13,6 +16,13 @@ async def create_user(
         user_create: UserCreateIn,
         session: AsyncSession = Depends(pg_helper.session_getter),
 ) -> UserBaseOut:
+    result = await session.execute(select(User).where(or_(User.login == user_create.login,User.email == user_create.email)))
+    existing_user = result.scalars().first()
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="User with this username or email already exists"
+        )
     user = await services_create_user(
         user_create=user_create,
         session=session,
