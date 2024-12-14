8fbd5668b0562ca822296eab400c89781459eb94
Nikita Mikhailov <maddog77100@gmail.com>
2024-12-14 15:44:11 +0300

    Добавил /me не доделал

--- src/api/auth.py
+++ src/api/auth.py
@@ -3,10 +3,12 @@ from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.postgres import pg_helper
-from schemas.user import UserBaseOut, UserCreateIn
-from services.users import create_user as services_create_user
+from schemas.user import UserBaseOut, UserCreateIn, UserAccountOut
+from services.users import create_user as services_create_user, account_page as service_account_page
+from pydantic import UUID4
 
 router = APIRouter()
+
 
 @router.post("/register", response_model=UserBaseOut)
 async def create_user(
@@ -15,6 +17,18 @@ async def create_user(
 ) -> UserBaseOut:
     user = await services_create_user(
         user_create=user_create,
+        session=session,
+    )
+    return user
+
+# TODO не доделал
+@router.post("/me", response_model=UserAccountOut)
+async def account_page(
+        user_id: UUID4,
+        session: AsyncSession = Depends(pg_helper.session_getter),
+) -> UserBaseOut:
+    user = await service_account_page(
+        user_id=user_id,
         session=session,
     )
     return user
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -5,13 +5,21 @@ from pydantic import UUID4, BaseModel, EmailStr
 
 class UserBaseOut(BaseModel):
     email: EmailStr
+
+
+class UserFullOut(UserBaseOut):
     first_name: str | None = None
     last_name: str | None = None
 
+
 class UserCreateIn(UserBaseOut):
     password: str
+
 
 class UserReadOut(UserBaseOut):
     id: UUID4
     created_at: datetime
 
+
+class UserAccountOut(UserBaseOut, UserCreateIn):
+    pass
