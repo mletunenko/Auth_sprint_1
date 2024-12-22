58b0f36b7e53b2c3dbe92d2fb63c0fe7379b5505
rock4ts <rock4ts@gmail.com>
2024-12-22 18:18:39 +0300

    Добавлены код и настройки консольной команды создания суперпользователя

--- src/core/config.py
+++ src/core/config.py
@@ -20,6 +20,7 @@ class RunConfig(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
+    sync_url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
