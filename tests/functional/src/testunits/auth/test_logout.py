e56b52f4ea7ecd7d20abe0270835447ee2ed8c9d
mletunenko <mletunenko@gmail.com>
2024-12-23 18:56:31 +0300

    тесты для ручек auth/register ./login ./refresh ./logout

---
+++ tests/functional/src/testunits/auth/test_logout.py
@@ -0,0 +1,9 @@
+
+
+def test_logout_success(client, clear_database, login_common_user):
+    access_token = login_common_user["access"]
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    logout_response = client.post("/auth/logout", headers=headers)
+    assert logout_response.status_code == 200
