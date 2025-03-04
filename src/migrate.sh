0fbdd8853015e54261d06b5b55299ec4be6e5973
mletunenko <mletunenko@gmail.com>
2025-03-04 13:19:46 +0300

    порт ПГ для скрипта миграций и примера env

--- src/migrate.sh
+++ src/migrate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-/scripts/wait-for-it.sh auth_postgres:5432 -s -t 60 &&
+/scripts/wait-for-it.sh auth_postgres:30001 -s -t 60 &&
 /scripts/wait-for-it.sh auth_redis:6379 -s -t 60 &&
 alembic upgrade head
